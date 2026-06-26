340a4dbaaaf935fa63124ee734fc05f1fec03d96
Felipe César <www.felipecesargomes@gmail.com>
2026-06-26 17:39:42 -0300

    feat: adicionar templates de tradução e resumo, e runnable para conversão de texto em número

---
+++ chains-e-processamentos/runnable-lambda.py
@@ -0,0 +1,7 @@
+from langchain_core.runnables import RunnableLambda
+def parse_number(text:str) -> int:
+    return int(text.strip())
+
+parse_number_runnable = RunnableLambda(parse_number)
+
+number = parse_number_runnable.invoke("10")
