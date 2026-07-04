c0e8035ec6fa306d810bd166de9a275898774a5b
Felipe César <www.felipecesargomes@gmail.com>
2026-07-04 18:41:31 -0300

    feat: adicionar novo pipeline de tradução e sumarização

---
+++ revisao/question_three_pipeline.py
@@ -0,0 +1,22 @@
+from langchain_core.prompts import PromptTemplate
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+
+from dotenv import load_dotenv
+load_dotenv()
+
+template_translate = PromptTemplate.from_template(
+    "Translate the following text to English:\n ```{initial_text}```"
+)
+
+template_summary = PromptTemplate.from_template(
+    "Summarize the following text in 4 words:\n ```{text}```"
+)
+
+llm_en = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+
+translate = template_translate | llm_en | StrOutputParser()
+pipeline = {"text": translate} | template_summary | llm_en | StrOutputParser()
+
+result = pipeline.invoke({"initial_text": "Langchain é um framework para desenvolvimento de aplicações de IA."})
+print(result)
